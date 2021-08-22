c45d4c608ab9f568a566e73e35bd3fee1b803474
Lakshyajit <lakshyajit165@gmail.com>
2021-08-22 22:59:39 +0530

    initial setup

---
+++ src/components/Posts.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { useGlobalContext } from "../contexts/GlobalContext";
+
+export default function Posts() {
+    const { posts, loading } = useGlobalContext();
+    
+    return (
+        <div>
+            <h2>Post Data</h2>
+            { 
+                loading ? <p>Loading...</p> : <></>
+            }
+            {
+                posts ? posts.map(post => <p key={post.id}>{post.title}</p>) : <></>
+            }
+        </div>
+    )
+}
---
+++ src/contexts/GlobalContext.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const GlobalContext = React.createContext();
+
+export function useGlobalContext() {
+    return useContext(GlobalContext);
+}
+
+export function GlobalContextProvider({ children }) {
+   const [posts, setPosts] = useState([]);
+   const [loading, setLoading] = useState(false);
+
+    function loadData() {
+        setLoading(true);
+        axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        .then(res => {
+            console.log(res);
+            setLoading(false);
+            setPosts(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    // we can add multiple useEffects(as per requirement)
+    useEffect(() => {
+        loadData();
+
+    }, [])
+
+    const value = {
+        posts,
+        loading,
+        setPosts,
+        loadData
+    }
+    
+    
+    return (
+        <GlobalContext.Provider value={value}>
+            { children }
+        </GlobalContext.Provider>
+    )
+}
